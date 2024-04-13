60524e02743a08adeddcc7e5b53df409400ee7ee
Jack McCaffrey <jackmccaffrey96@gmail.com>
2024-04-13 13:52:37 +0100

    Added StateChart and ported movement/cleansing logic to states

--- src/entities/characters/crusader/crusader.gd
+++ src/entities/characters/crusader/crusader.gd
@@ -3,6 +3,8 @@ class_name Crusader
 
 signal cleansing_complete
 
+@onready var state_chart: StateChart = $StateChart
+
 var path: Curve2D
 var path_points: PackedVector2Array
 var path_index: int = 0:
@@ -16,37 +18,20 @@ var ritual_point:
 		ritual_point = value
 
 
-func _move(delta):
-	# If we're in range of a ritual point, move to that
-	if ritual_point:
-		nav_agent.target_position = ritual_point
-	else:
-		# Otherwise we move to the next path node
-		nav_agent.target_position = path_points[path_index]
-	
-	if nav_agent.is_navigation_finished():
-		if ritual_point:
-			return
-		path_index += 1
-		nav_agent.target_position = path_points[path_index]
-		return
-	
-	var current_agent_position: Vector2 = global_position
-	var next_path_position: Vector2 = nav_agent.get_next_path_position()
-	var direction: Vector2 = current_agent_position.direction_to(next_path_position)
-	var intended_velocity: Vector2 = direction * speed
-	nav_agent.set_velocity(intended_velocity)
+func _ready():
+	# TODO - play some animation or effect before beginning the movement
+	pass
 
 
 func start_cleanse(ritual_node):
 	cleansing_complete.connect(ritual_node.cleanse)
-	await get_tree().create_timer(1.5).timeout
-	emit_signal("cleansing_complete")
-	finish_cleanse()
+	state_chart.send_event("stop_walking")
+	state_chart.send_event("start_cleansing")
 
 
 func finish_cleanse():
 	ritual_point = null
+	emit_signal("cleansing_complete")
 	
 	var path_points_sorted_closest = Array(path_points)
 	path_points_sorted_closest.sort_custom(
@@ -59,3 +44,44 @@ func finish_cleanse():
 	
 	path_index = path_points.find(closest_path_point)
 	nav_agent.target_position = closest_path_point
+	
+	state_chart.send_event("stop_cleansing")
+
+
+func _on_idle_state_physics_processing(delta):
+	if nav_agent.target_position:
+		state_chart.send_event("start_walking")
+	return
+
+
+func _on_walking_state_physics_processing(delta):
+	# If we're in range of a ritual point, move to that
+	if ritual_point:
+		nav_agent.target_position = ritual_point
+	else:
+		# Otherwise we move to the next path node
+		nav_agent.target_position = path_points[path_index]
+	
+	if nav_agent.is_navigation_finished():
+		if ritual_point:
+			state_chart.send_event("stop_walking")
+			return
+		path_index += 1
+		nav_agent.target_position = path_points[path_index]
+		return
+	
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = nav_agent.get_next_path_position()
+	var direction: Vector2 = current_agent_position.direction_to(next_path_position)
+	var intended_velocity: Vector2 = direction * speed
+	nav_agent.set_velocity(intended_velocity)
+
+
+func _on_action_idle_state_entered():
+	state_chart.send_event("start_walking")
+
+
+func _on_action_cleansing_state_entered():
+	await get_tree().create_timer(1.5).timeout
+	finish_cleanse()
+
