c6ee93da9f412444b58ea5beab852df22ce996ef
NKLam <khoalamvn@gmail.com>
2024-04-21 09:45:40 +1000

    Make spellbook UI show buttons depend on spell resource

--- src/levels/main_scene/spell_book.gd
+++ src/levels/main_scene/spell_book.gd
@@ -1,11 +1,23 @@
 extends Control
 
+@export var prefix_data: Array[SpellPrefixResource]
+@export var spell_data: Array[SpellMainResource]
+@export var spellbook_button_prefab: PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var grid_container: GridContainer = $TabContainer/Overview/ScrollContainer/GridContainer
 
+func _ready():
+    for child in grid_container.get_children():
+        child.queue_free()
+    create_buttons()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func create_buttons():
+    for elem in prefix_data:
+        var new_btn = spellbook_button_prefab.instantiate()
+        grid_container.add_child(new_btn)
+        new_btn.populate_prefix_data(elem)
+
+    for elem in spell_data:
+        var new_btn = spellbook_button_prefab.instantiate()
+        grid_container.add_child(new_btn)
+        new_btn.populate_spell_data(elem) 
--- src/levels/main_scene/spellbook_button.gd
+++ src/levels/main_scene/spellbook_button.gd
@@ -1,16 +1,24 @@
 extends Button
 class_name SpellbookButton
 
-@onready var monster_image: TextureRect = $TextureRect
+@onready var monster_image: TextureRect = $MonsterImage
+@onready var prefix_text: RichTextLabel = $PrefixText
 
 var spell_data: SpellMainResource
 var prefix_data: SpellPrefixResource
 
+func _ready():
+	text = ""
+	monster_image.texture = null
+	prefix_text.text = ""
+
+
 func populate_spell_data(data: SpellMainResource):
 	spell_data = data
 	prefix_data = null
-	set("theme_override_colors/font_color", Color.GREEN)
-	text = spell_data.name
+	monster_image.texture = data.monster_sprite
+	# set("theme_override_colors/font_color", Color.GREEN)
+	# text = spell_data.name
 
 func populate_prefix_data(data: SpellPrefixResource):
 	spell_data = null
