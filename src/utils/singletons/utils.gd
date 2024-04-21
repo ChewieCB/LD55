24981d848b8c2a6beed1fae18c99b0adcd6efb2d
NKLam <khoalamvn@gmail.com>
2024-04-21 16:18:24 +1000

    Fix arrow sprite missing

--- src/utils/singletons/utils.gd
+++ src/utils/singletons/utils.gd
@@ -8,13 +8,13 @@ func convert_text_to_arrow(text: String, color_str: String="", size: int=30):
 	for character in text:
 		match character:
 			"U":
-				result += "[img={0} color={1}]res://assets/sprite/arrow_up.png[/img]".format([size, color_str])
+				result += "[img={0} color={1}]res://assets/ui/arrow_up.png[/img]".format([size, color_str])
 			"L":
-				result += "[img={0} color={1}]res://assets/sprite/arrow_left.png[/img]".format([size, color_str])
+				result += "[img={0} color={1}]res://assets/ui/arrow_left.png[/img]".format([size, color_str])
 			"R":
-				result += "[img={0} color={1}]res://assets/sprite/arrow_right.png[/img]".format([size, color_str])
+				result += "[img={0} color={1}]res://assets/ui/arrow_right.png[/img]".format([size, color_str])
 			"D":
-				result += "[img={0} color={1}]res://assets/sprite/arrow_down.png[/img]".format([size, color_str])
+				result += "[img={0} color={1}]res://assets/ui/arrow_down.png[/img]".format([size, color_str])
 	return result
 
 func play_button_click_sfx():
